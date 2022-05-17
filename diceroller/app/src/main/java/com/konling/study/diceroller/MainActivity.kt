3f0345ea4ed5430a92b88826b907a8bf1f938fff
尹蒙 <yinmeng@zhiruibao.com>
2022-05-17 16:38:48 +0800

    布局及基础响应

--- diceroller/app/src/main/java/com/konling/study/diceroller/MainActivity.kt
+++ diceroller/app/src/main/java/com/konling/study/diceroller/MainActivity.kt
@@ -2,10 +2,19 @@ package com.konling.study.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rollbutton: Button = findViewById(R.id.roll_button)
+        rollbutton.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
     }
 }
