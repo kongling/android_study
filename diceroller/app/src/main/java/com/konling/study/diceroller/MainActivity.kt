90d5eba87fb4fcba3e7429db3956b9acf945eee7
尹蒙 <yinmeng@zhiruibao.com>
2022-05-19 16:39:58 +0800

    Lesson 01.3 Image resources and compatibility Done

--- diceroller/app/src/main/java/com/konling/study/diceroller/MainActivity.kt
+++ diceroller/app/src/main/java/com/konling/study/diceroller/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.konling.study.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
         var rollbutton: Button = findViewById(R.id.roll_button)
         rollbutton.setOnClickListener { rollDice() }
 
     }
 
     private fun rollDice() {
-        var randomInt = (1 .. 6).random();
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
-        var resultText : TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomdiceImage())
+        diceImage2.setImageResource(getRandomdiceImage())
 
+    }
+
+    private fun getRandomdiceImage():Int{
+        var randomInt = (1 .. 6).random()
+        var drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
     }
 }
